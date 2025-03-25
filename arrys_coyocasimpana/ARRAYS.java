36f6308961593c98135cf680cf78eb44d2ab30ed
CoyocaNico <coyocanico85@gmail.com>
2025-03-25 08:45:11 +0800

    Add files via upload

---
+++ arrys_coyocasimpana/ARRAYS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrys_coyocasimpana;
+
+/**
+ *
+ * @author User
+ */
+public class ARRAYS {
+  
+   public static void main(String[] args){
+     String [] array = {"  A, B, C, D, E,F, G" } ;
+     for (String letters : array ) {
+         
+        System.out.print ( letters + "");
+     }
+      
+     
+       
+      
+   
+   }
+    
+}
