513a637d2a55cfd4d08e3f08257ef79ba9e701ed
CoyocaNico <coyocanico85@gmail.com>
2025-03-25 08:37:57 +0800

    Add files via upload

---
+++ methods_coyoca_simpana/BASIC_CACULATOR.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package methods_coyoca_simpana;
+
+import java.util.Scanner;
+
+
+public class BASIC_CACULATOR {
+    
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+    public static double subtraction(double a, double b) {
+        return a - b;
+    }
+    public static double multiplication (double a, double b) {
+        return a * b;
+    }
+    public static double division (double a, double b) {
+        return a / b;
+    }
+    public static double percentage(double a, double b) {
+        if (b==0) {
+       return a/b;
+       }else {
+         throw new ArithmeticException("cannot devide by zero");  
+       }
+    
+    }
+    public double percent(double a, double b,) {
+        return (a /100) *b;
+    }
+    public static void main 
+
+
+wla pa ma chek ni maam
+        
+    
+   
